1be0930811305ecb2f8b03921e74f580c4a22d6a
Siddhartha Narang <siddhartha.narang@gmail.com>
2020-10-08 19:35:21 +0530

    Implementing push solution in a Doubly Linked List

---
+++ doublyLinkedList/doublyLinkedListPush.js
@@ -0,0 +1,39 @@
+class Node {
+  constructor(val){
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor(){
+    this.head = null;
+    this.tail  = null;
+    this.length = 0;
+  }
+
+  push(val){
+    const newNode = new Node(val);
+    this.length++;
+    if(!this.head){
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    else{
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    return this;
+  }
+}
+
+let list = new DoublyLinkedList();
+
+console.log(`Push element into the Doubly Linked List `,list.push(1));
+console.log(`Push element into the Doubly Linked List `,list.push(2));
+console.log(`Push element into the Doubly Linked List `,list.push(3));
+console.log(`Push element into the Doubly Linked List `,list.push(4));
+
+console.log(`List is `,list);
