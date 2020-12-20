7c4265ab91c676a23943799f0fc4f5af3e4387b3
Siddhartha Narang <siddhartha.narang@gmail.com>
2020-12-21 02:15:49 +0530

    Implementinh hash map keys and values

--- hashTable/hashMapKeysAndValues.js
+++ hashTable/hashMapKeysAndValues.js
@@ -1,16 +1,17 @@
-// Implementing Hash tabled Set and Get using our own class of Hash table with Separate Chaining collision Technique
+// Implementing Hash Table Keys And Values using HashMap Class
 
 class HashMap{
-  constructor(size = 13){
+  constructor(size=53){
     this.keyMap = new Array(size);
   }
   
   _hash(key){
+    
     let WEIRD_PRIME = 31;
     let total =0, value;
-    for(let i=0; i<Math.min(key.length, 100);i++){
-      value = key[i].charCodeAt(0) - 96;
-      total = (total * WEIRD_PRIME + value) % this.keyMap.length
+    for(let i=0; i<Math.min(key.length, 100); i++){
+      let value = key[i].charCodeAt(0);
+      total = (total * WEIRD_PRIME + value)%this.keyMap.length;
     }
     return total;
   }
@@ -35,12 +36,40 @@ class HashMap{
     if(!this.keyMap[hashValue]){
       return;
     }
-    for(let i=0; i<this.keyMap[hashValue].length; i++){
-      if(key === this.keyMap[hashValue][i][0]){
+    for(let i=0; i< this.keyMap[hashValue].length; i++){
+      if(this.keyMap[hashValue][i][0] === key){
         return this.keyMap[hashValue][i][1];
       }
     }
     return;
+  }
+  
+  keys(){
+    let dummyArray = [];
+    for(let i=0; i<this.keyMap.length; i++){
+      if(!this.keyMap[i]){
+        continue;
+      }
+      for(let j=0; j<this.keyMap[i].length; j++){
+        dummyArray.push(this.keyMap[i][j][0]);
+      }
+    }
+    return dummyArray;
+  }
+  
+  values(){
+    let dummyArray = [];
+    for(let i=0; i<this.keyMap.length; i++){
+      if(!this.keyMap[i]){
+        continue;
+      }
+      for(let j=0; j<this.keyMap[i].length; j++){
+        if(dummyArray.indexOf(this.keyMap[i][j][1]) === -1){
+          dummyArray.push(this.keyMap[i][j][1]);
+        }
+      }
+    }
+    return dummyArray;
   }
 }
 
@@ -50,6 +79,7 @@ console.log('Set value in the hash map ', map.set('a',1));
 console.log('Set value in the hash map ', map.set('b',2));
 console.log('Set value in the hash map ', map.set('c',3));
 console.log('Set value in the hash map ', map.set('d',4));
+console.log('Set value in the hash map ', map.set('d',5));
 console.log('Set value in the hash map ', map.set('pink','Value of pink'));
 console.log('Set value in the hash map ', map.set('cyan','Value of cyan'));
 
@@ -58,3 +88,7 @@ console.log('Get value in the hash map ', map.get('pink'));
 console.log('Get value in the hash map ', map.get('a'));
 console.log('Get value in the hash map ', map.get('e'));
 console.log('Get value in the hash map ', map.get('k'));
+
+console.log('Keys of hash map is ', map.keys());
+console.log('Keys of hash map is ', map.values());
+
