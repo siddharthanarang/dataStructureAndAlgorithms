64524f08ad9c108e74dcd1b313af3e537013d326
Siddhartha Narang <siddhartha.narang@gmail.com>
2020-12-31 21:01:50 +0530

    Implementinh Add vertex of an Graph

---
+++ graph/addVertex.js
@@ -0,0 +1,19 @@
+class Graph {
+  constructor(){
+    this.adjacencyList = {};
+  }
+  
+  addVertex(vertex){
+    if(!this.adjacencyList[vertex]){
+      this.adjacencyList[vertex] = [];
+    }
+    return this;
+  }
+}
+
+let letters = new Graph();
+
+console.log(`Add vertex in Graph `, letters.addVertex('A'));
+console.log(`Add vertex in Graph `, letters.addVertex('B'));
+
+console.log(`Graph is `, letters);
