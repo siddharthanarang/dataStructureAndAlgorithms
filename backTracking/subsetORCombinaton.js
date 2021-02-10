62683f2410c6e357658f1cba125758573decfdb2
Siddhartha Narang <siddhartha.narang@gmail.com>
2021-02-10 21:53:29 +0530

    Revised soluyion of Combination of Array

--- backTracking/subsetORCombinaton.js
+++ backTracking/subsetORCombinaton.js
@@ -1,20 +1,33 @@
-let numbers = [2,3,5];
+let numbers = [1,2,3];
 
-const combination = (numbers, result = []) => {
-  
-  if(!numbers || !numbers.length){
-    console.log(`Result is `,result);
+// const combination = (numbers, result = []) => {
+//  
+//   if(!numbers || !numbers.length){
+//     console.log(`Result is `,result);
+//     return;
+//   }
+//  
+//   let currentNumbers = numbers.shift();
+//  
+//   //No is not included
+//   combination(JSON.parse(JSON.stringify(numbers)), JSON.parse(JSON.stringify(result)));
+//
+//   //No is included
+//   result.push(currentNumbers);
+//   combination(JSON.parse(JSON.stringify(numbers)), JSON.parse(JSON.stringify(result)));
+//  
+// };
+
+let combination = (numbers, currentIndex = 0,currentPosition =0, data = []) => {
+  if(currentPosition >= numbers.length){
+    console.log(`Data is `,data.slice(0,currentIndex));
     return;
   }
   
-  let currentNumbers = numbers.shift();
+  data[currentIndex] = numbers[currentPosition];
+  combination(numbers, currentIndex+1, currentPosition+1, data);
+  combination(numbers, currentIndex, currentPosition+1, data);
   
-  //No is not included
-  combination(JSON.parse(JSON.stringify(numbers)), JSON.parse(JSON.stringify(result)));
-
-  //No is included
-  result.push(currentNumbers);
-  combination(JSON.parse(JSON.stringify(numbers)), JSON.parse(JSON.stringify(result)));
   
 };
 
